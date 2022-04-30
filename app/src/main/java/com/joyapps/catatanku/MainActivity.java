069e29b92138ae107a4688e09bea0b70a131722c
ahmadyogi543 <ahmadyogi543@gmail.com>
2022-04-30 14:32:45 +0800

    first commit

---
+++ app/src/main/java/com/joyapps/catatanku/MainActivity.java
@@ -0,0 +1,24 @@
+package com.joyapps.catatanku;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // menginisialisasi bottom navigation view
+        BottomNavigationView bottomNavigationView = findViewById(R.id.mainActivity_bottomNavView);
+        NavController navController = Navigation.findNavController(findViewById(R.id.mainActivity_navFragment));
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
